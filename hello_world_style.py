d707d058071495ba59cb98d16ee9da5fbc2697da
Reid MacLean <reid.maclean@stu.ocsb.ca>
2025-02-13 01:12:38 +0000

    Commit message

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By: Reid MacLean
+# Date: Feb. 12, 2025
+# This program displays Hello, World! with proper style. 
+
+
+def main ():
+    print("Hello World! with proper style!")
+    print("I am testing my function here.")
+
+
+if __name__ == "__main__":
+    main()
